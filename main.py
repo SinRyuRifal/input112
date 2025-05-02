f48c45d212a5834db3d1e23d44801acdb2de2d97
SinRyuRifal <rifqii.naufall37@gmail.com>
2025-05-02 18:59:18 +0700

    req

--- main.py
+++ main.py
@@ -7,15 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import io
-import undetected_chromedriver as uc
+
 
 def setup_driver():
-    options = uc.ChromeOptions()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = uc.Chrome(options=options)
-    return driver
+    options = webdriver.ChromeOptions()
+    
+    return webdriver.Chrome(options=options)
 
 
 def process_excel(file):
